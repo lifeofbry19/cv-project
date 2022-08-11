a2b6fd8bff4f797e8a9931c668ababae3282a035
Bryant Young <thebryisonfire@gmail.com>
2022-08-10 22:13:54 -0500

    initial home page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ResumeTemplate1 from "../ResumeTemplate1.png";
+import HomeImg from "../HomeImg.jpg";
 
 const Home = () => {
   return (
     <div className="home-page">
       <nav className="home-header">Resume Builder</nav>
       <div className="welcome-text">
-        <h2>Welcome to Resume Builder! Choose a template to get started.</h2>
+        <h1>Welcome to Resume Builder! This is where you get the job.</h1>
+        <p>
+          A Quick and Easy Way to Create Your Professional Resume. 30+
+          Professional Resume Templates Choose from over thirty modern and
+          professional templates. All of which can be customized to your liking.
+          Fast and Easy to Use Our resume builder lets you easily and quickly
+          create a resume using our resume wizard.
+        </p>
+      </div>
+      <div className="home-img-wrapper">
+        <img className="home-image" src={HomeImg} alt="" />
       </div>
       <div className="templates">
+        <h1>Choose a template to get started</h1>
         <a href="/resume-builder">
           <img src={ResumeTemplate1} alt="" />
         </a>
